5b153d3ae07b473c0bad736c252f357ab1c01395
unknown <Administrator@BG4NB1284.SONATA.LOCAL>
2026-01-27 21:59:57 +0530

    env file added and made fucntion to extract playlistID

---
+++ video_stats.py
@@ -0,0 +1,29 @@
+import requests as r
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+BASE_URL=os.getenv("BASE_URL")
+channel_handle = 'MrBeast'
+
+def get_channel_playlist_id():
+    try:
+        url=f'{BASE_URL}{channel_handle}&key={API_KEY}'
+        response = r.get(url)
+        response.raise_for_status()
+        data = response.json()
+
+        channle_items=data["items"][0]
+
+        channel_playlistId=channle_items["contentDetails"]["relatedPlaylists"]["uploads"]
+        print(channel_playlistId)
+        return channel_playlistId
+    except r.RequestException as e:
+        print(f"Error fetching channel playlist ID: {e}")
+        return None
+
+if __name__ == "__main__":
+   get_channel_playlist_id()
